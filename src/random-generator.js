1f328c8f3865e4b195151cebf93b4b7b6d4574a5
Si1adan <mark.pyankov@mail.ru>
2022-12-31 01:01:37 +0600

    add new files

---
+++ src/random-generator.js
@@ -0,0 +1,6 @@
+const getRandomIntInclusive = (minimum, maximum) => {
+  const min = Math.ceil(minimum);
+  const max = Math.floor(maximum);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+export default getRandomIntInclusive;
