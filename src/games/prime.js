efa87873963b4466ee1846588e7c1fc6fc554585
Si1adan <mark.pyankov@mail.ru>
2023-01-08 19:05:06 +0600

    prep for brain-prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,17 @@
+import readlineSync from 'readline-sync';
+import { getRandomInt } from '../random-generator.js';
+import * as engine from '../index.js';
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
