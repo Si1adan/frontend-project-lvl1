26253fa5f032fac494352db1c107bd30a1c13a5f
Si1adan <mark.pyankov@mail.ru>
2023-01-09 17:12:48 +0600

    add brain-prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,17 +1,34 @@
 import readlineSync from 'readline-sync';
-import { getRandomInt } from '../random-generator.js';
+import { getRandomInt, isPrime } from '../random-generator.js';
 import * as engine from '../index.js';
 
-const isPrime = (num) => {
-  if (num < 2) {
-    return false;
-  }
+const prime = () => {
+  const userName = engine.welcome();
+  engine.rules('Answer "yes" if given number is prime. Otherwise answer "no".');
 
-  for (let i = 2; i <= num / 2; i += 1) {
-    if (num % i === 0) {
-      return false;
+  const iter = () => {
+    const roundsCount = 3;
+
+    for (let i = 0; i < roundsCount; i += 1) {
+      const num = getRandomInt();
+      const correctAnswer = isPrime(num) ? 'yes' : 'no';
+      const exp = num;
+
+      const userAnswer = readlineSync.question(`Question: ${exp}\nYour answer: `).trim().toLowerCase();
+
+      if (correctAnswer.toString() === userAnswer) {
+        console.log('Correct!');
+      } else {
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+        console.log(`Let's try again, ${userName}!`);
+        return;
+      }
     }
-  }
 
-  return true;
+    console.log(`Congratulations, ${userName}!`);
+  };
+
+  iter();
 };
+
+export default prime;
--- src/random-generator.js
+++ src/random-generator.js
@@ -7,3 +7,16 @@ export const getRandomOperator = (arrayOfOperators = ['+', '-', '*']) => {
   const index = Math.floor(Math.random() * arrayOfOperators.length);
   return arrayOfOperators[index];
 };
+export const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
