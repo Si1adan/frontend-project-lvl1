c3bd34635efd9d6c10b290bbcce23d7616b7790e
Si1adan <mark.pyankov@mail.ru>
2023-01-20 00:10:21 +0600

    update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,43 +1,31 @@
-import readlineSync from 'readline-sync';
-import { getRandomInt } from '../random-generator.js';
 import * as engine from '../index.js';
+import { getRandomInt } from '../utils.js';
 
-const progression = () => {
-  const userName = engine.welcome();
-  engine.rules('What number is missing in the progression?');
+export default () => {
+  const rules = 'What number is missing in the progression?';
+  const userName = engine.welcome(rules);
 
-  const iter = () => {
-    const roundsCount = 3;
-    const arrLength = 10;
+  for (let i = 0; i < engine.roundsCount; i += 1) {
+    const initNum = getRandomInt(0, 10);
+    const diff = getRandomInt(1, 10);
+    const emptyPos = getRandomInt(0, 10);
+    const row = [initNum];
+    const rowLength = 10;
 
-    for (let i = 0; i < roundsCount; i += 1) {
-      const initNum = getRandomInt(0, 10);
-      const diff = getRandomInt(1, 10);
-      const emptyPos = getRandomInt(0, 10);
-      const row = [initNum];
-
-      for (let j = 1; j <= arrLength; j += 1) {
-        row.push(initNum + j * diff);
-      }
-
-      const correctAnswer = row[emptyPos];
-      row[emptyPos] = '..';
-      const exp = row.toString().replaceAll(',', ' ');
-      const userAnswer = readlineSync.question(`Question: ${exp}\nYour answer: `).trim().toLowerCase();
-
-      if (correctAnswer.toString() === userAnswer) {
-        console.log('Correct!');
-      } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-        console.log(`Let's try again, ${userName}!`);
-        return;
-      }
+    for (let j = 1; j <= rowLength; j += 1) {
+      row.push(initNum + j * diff);
     }
 
-    console.log(`Congratulations, ${userName}!`);
-  };
+    const correctAnswer = row[emptyPos];
+    row[emptyPos] = '..';
+    const exp = row.toString().replaceAll(',', ' ');
 
-  iter();
+    const userAnswer = engine.askQuestion(exp);
+    const endFlag = engine.checkAnswer(userName, correctAnswer, userAnswer);
+    if (endFlag) {
+      return;
+    }
+  }
+
+  engine.end(userName);
 };
-
-export default progression;
