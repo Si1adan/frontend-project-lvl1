0123441eaf7583fa649fbc460780e40adb9648e3
Si1adan <mark.pyankov@mail.ru>
2023-01-20 00:09:36 +0600

    update calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,9 +1,26 @@
 import * as engine from '../index.js';
+import {
+  getCalcCorrectAnswer, getCalcExp, getRandomInt, getRandomOperator,
+} from '../utils.js';
 
-const calc = () => {
-  const userName = engine.welcome();
-  engine.rules('What is the result of the expression?');
-  engine.iterations(userName);
+export default () => {
+  const rules = 'What is the result of the expression?';
+  const userName = engine.welcome(rules);
+
+  for (let i = 0; i < engine.roundsCount; i += 1) {
+    const num1 = getRandomInt(1, 50);
+    const num2 = getRandomInt(1, 50);
+    const operator = getRandomOperator();
+
+    const exp = getCalcExp(num1, num1, operator);
+    const correctAnswer = getCalcCorrectAnswer(num1, num2, operator);
+
+    const userAnswer = engine.askQuestion(exp);
+    const endFlag = engine.checkAnswer(userName, correctAnswer, userAnswer);
+    if (endFlag) {
+      return;
+    }
+  }
+
+  engine.end(userName);
 };
-
-export default calc;
