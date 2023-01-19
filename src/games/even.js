3ccdc58dbad96cd0f95f089cc07008b6e7f5f9a9
Si1adan <mark.pyankov@mail.ru>
2023-01-20 00:09:51 +0600

    update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,30 +1,20 @@
-import readlineSync from 'readline-sync';
-import getRandomInt from '../random-generator.js';
+import * as engine from '../index.js';
+import { isEven, getRandomInt } from '../utils.js';
 
-const even = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+export default () => {
+  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const userName = engine.welcome(rules);
 
-  const isEven = (num) => (num % 2 === 0);
-  const roundsCount = 3;
+  for (let i = 0; i < engine.roundsCount; i += 1) {
+    const exp = getRandomInt();
+    const correctAnswer = isEven(exp) ? 'yes' : 'no';
 
-  for (let i = 0; i < roundsCount; i += 1) {
-    const num = getRandomInt(1, 100);
-    const userAnswer = readlineSync.question(`Question: ${num}\nYour answer: `).trim().toLowerCase();
-    const correctAnswer = isEven(num) ? 'yes' : 'no';
-    const wrongAnswer = !isEven(num) ? 'yes' : 'no';
-
-    if (correctAnswer === userAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${wrongAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
+    const userAnswer = engine.askQuestion(exp);
+    const endFlag = engine.checkAnswer(userName, correctAnswer, userAnswer);
+    if (endFlag) {
       return;
     }
   }
-  console.log(`Congratulations, ${userName}!`);
-};
 
-export default even;
+  engine.end(userName);
+};
